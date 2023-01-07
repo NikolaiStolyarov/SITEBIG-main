1f4f105f46f11ba1ef842550e60f8ae75c886703
Nikolai <deadliner.nik@gmail.com>
2023-01-07 13:42:16 +0300

    Final

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -3,8 +3,24 @@ export function menus()
 console.log('menuwork');
 const currentPage = window.location.pathname;
 console.log( "Название страницы" , currentPage);
+
+if (currentPage === "/works.html") {
+  const menu = document.querySelectorAll('.menu__link')[0];
+  menu.classList.toggle('_active');
+}
+if (currentPage === "/article.html") {
+  const menu = document.querySelectorAll('.menu__link')[0];
+  menu.classList.toggle('_active');
+}
+
 if (currentPage === "/blog.html") {
   const menu = document.querySelectorAll('.menu__link')[1];
   menu.classList.toggle('_active');
 }
+
+if (currentPage === "/index.html") {
+  const menu = document.querySelectorAll('.menu__link')[2];
+  menu.classList.toggle('_active');
+}
+
 }
